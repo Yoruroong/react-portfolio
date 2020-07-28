891ac56a4cb29528f265761e46241db3b2f72dea
Yoruroong <tuntun4132@naver.com>
2020-07-28 10:29:23 +0900

    icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p class="big">Yoru</p>
-        <p>개발하고있으니조금만기다려주세요!</p>
+        <Button
+          style={{
+            backgroundColor: 'whitesmoke',
+          }}
+          onClick={(e) => {
+          e.preventDefault();
+          window.location.href='http://github.com/Yoruroong';
+        }}
+        > <GitHubIcon color="black"/></Button>
       </header>
     </div>
   );
