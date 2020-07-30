1e07773b01131a6f421bedfd4d4119af4d29ad0d
Yoruroong <tuntun4132@naver.com>
2020-07-30 20:53:13 +0900

    bold

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p class="big">안녕하세요,<br />저는 요루라고 해요.</p>
+        <p class="big bold">안녕하세요,<br />저는 요루라고 해요.</p>
         <div style={{float: 'left', margin:'-15px'}}>
           <a href='http://github.com/Yoruroong' style={{color: 'whitesmoke', marginRight:'10px'}}><GitHubIcon color="black"/></a>
           <a href="mailto:@yoru@outlook.kr" style={{color: 'whitesmoke'}}><MailIcon color="black" /></a>
