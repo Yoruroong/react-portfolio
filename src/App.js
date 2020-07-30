c0e85b8a60b0c88ac7c7a4b02345a5adab6ec5c5
Yoruroong <tuntun4132@naver.com>
2020-07-30 10:14:06 +0900

    icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import './App.css';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import Button from '@material-ui/core/Button';
+import MailIcon from '@material-ui/icons/Mail';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p class="big">안녕하세요,<br />저는 요루라고 해요.</p>
-        <Button
-          style={{
-            backgroundColor: 'whitesmoke',
-          }}
-          onClick={(e) => {
-          e.preventDefault();
-          window.location.href='http://github.com/Yoruroong';
-        }}
-        > <GitHubIcon color="black"/></Button>
+        <div style={{float: 'left', margin:'-15px'}}>
+          <a href='http://github.com/Yoruroong' style={{color: 'whitesmoke', marginRight:'10px'}}><GitHubIcon color="black"/></a>
+          <a href="mailto@yoru@outlook.kr" style={{color: 'whitesmoke'}}><MailIcon color="black" /></a>
+        </div>
       </header>
     </div>
   );
