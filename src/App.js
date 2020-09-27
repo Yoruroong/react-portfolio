b29314be64030a071d356b5afbec93e5d99d8d62
Yoruroong <tuntun4132@naver.com>
2020-09-27 03:34:03 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import MailIcon from '@material-ui/icons/Mail';
+import First from './components/First'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p class="big bold">안녕하세요,<br />저는 요루라고 해요.</p>
-        <div style={{float: 'left', margin:'-15px'}}>
-          <a href='http://github.com/Yoruroong' style={{color: 'whitesmoke', marginRight:'10px'}}><GitHubIcon color="black"/></a>
-          <a href="mailto:yoru@outlook.kr" style={{color: 'whitesmoke'}}><MailIcon color="black" /></a>
-        </div>
+        <First />
       </header>
     </div>
   );
