e44279590d6833e05fff937fbf67d6b4ba24b546
Yoruroong <tuntun4132@naver.com>
2021-02-01 16:44:06 +0900

    asdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <footer style={{ height: '50px', backgroundColor: 'whitesmoke', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-        <p>asdf</p>
+        <p>© 2018-{new Date().getFullYear()} Yoru. 모든 권리 보유</p>
     </footer>
   );
 }
